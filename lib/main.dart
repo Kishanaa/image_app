a135ddfe3f0ff56f2d83d53d8256d85166a4e17b
Kishanaa <Kishanshrivastav18@gmail.com>
2025-11-26 23:49:13 +0530

    assests image and url image

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: Center(child: Image.asset('assets/gallery.png'))),
+      home: Scaffold(
+        body: Row(
+          children: <Widget>[
+            Image.asset('assets/gallery.png'),
+            Image.network(
+              'https://i.pinimg.com/736x/9f/6b/dd/9f6bdd17a91a7fae9c14b92dde54f567.jpg',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-//hello 
